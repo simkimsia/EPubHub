65976539c1e53e764d654e56fbac3aa018e3bd9e
kimsia <kimcity@gmail.com>
2013-03-19 14:56:24 +0800

    using pathname to calculate mimetype and name

--- lib/EPubHub/Page/FixedLayout.php
+++ lib/EPubHub/Page/FixedLayout.php
@@ -17,22 +17,61 @@
  */
 class EPubHub_Page_FixedLayout implements EPubHub_PageInterface
 {
-    protected $image      = '';
-    protected $folderPath = '';
-    protected $mimeType   = '';
+    protected $name     = '';
+    protected $mimeType = '';
+    protected $path     = '';
+    protected $size     = 0;
 
-    public function __construct($image, $mimeType = 'image/jpeg')
+    // for more info on mimetypes, check http://www.php.net/manual/en/function.image-type-to-mime-type.php
+    protected $approvedMimeTypes = array(
+        'image/gif',
+        'image/jpeg',
+        'image/png',
+    );
+
+    public function __construct($path)
     {
-        $this->image    = $image;
-        $this->mimeType = $mimeType;
+        $this->path = $path;
+        $this->updateMIMEType();
+        $this->updateName();
+    }
+
+    public function updateMIMEType()
+    {
+        $file       = $this->path;
+        $file_info  = new finfo(FILEINFO_MIME);  // object oriented approach!
+        $mime_type  = $file_info->buffer(file_get_contents($file));  // e.g. gives "image/jpeg"
+
+        if (!in_array($mime_type, $this->approvedMimeTypes))
+        {
+            // throw exception
+        }
+        $this->mimeType = $mime_type;
     }
 
     public function getMIMEType()
     {
-        $file       = $this->folderPath . DIRECTORY_SEPARATOR . $this->image;
-        $file_info  = new finfo(FILEINFO_MIME);  // object oriented approach!
-        $mime_type  = $file_info->buffer(file_get_contents($file));  // e.g. gives "image/jpeg"
+        return $this->mimeType;
+    }
 
-        return $mime_type;
+    public function updateName()
+    {
+        $this->name = basename($this->path);
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setPath($path)
+    {
+        if (!file_exists($path))
+        {
+            // throw exception
+        }
+        $this->path = $path;
+        $this->updateName();
+        $this->updateMIMEType();
     }
 }
