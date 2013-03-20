77bde67afa2cf126394729a3e375cba17233031c
kimsia <kimcity@gmail.com>
2013-03-20 17:28:52 +0800

    add in title and id for page

--- lib/EPubHub/Page/FixedLayout.php
+++ lib/EPubHub/Page/FixedLayout.php
@@ -18,9 +18,18 @@
 class EPubHub_Page_FixedLayout implements EPubHub_PageInterface
 {
     protected $image = null;
+    protected $title = '';
+    protected $id    = '';
 
-    public function __construct($image)
+    /**
+     * Constructor for EPubHub_Page_FixedLayout
+     *
+     * @return EPubHub_Page_FixedLayout object
+     */
+    public function __construct($image, $options = array())
     {
+        $this->title = isset($options['title']) ? $options['title'] : '';
+        $this->id    = isset($options['id']) ? $options['id'] : '';
         $this->image = $image;
     }
 
@@ -34,8 +43,83 @@ class EPubHub_Page_FixedLayout implements EPubHub_PageInterface
         return $this->image;
     }
 
+    /**
+     *
+     * Alias for getImage() 
+     * @return EPubHub_Image_FixedLayout object
+     */
+    public function image()
+    {
+        return $this->getImage();
+    }
+
+    /**
+     * Set image object
+     *
+     * @param EPubHub_Image_FixedLayout $image
+     */
     public function setImage(EPubHub_Image_FixedLayout $image)
     {
         $this->image = $image;
+    }
+
+    /**
+     * Get title 
+     *
+     * @return string title
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     *
+     * Alias for getTitle() 
+     * @return string
+     */
+    public function title()
+    {
+        return $this->getTitle();
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get id 
+     *
+     * @return string id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     *
+     * Alias for getId() 
+     * @return string
+     */
+    public function id()
+    {
+        return $this->getId();
+    }
+
+    /**
+     * Set id
+     *
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
     }
 }
